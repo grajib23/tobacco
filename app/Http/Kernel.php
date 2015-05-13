51743f002e9af9e6b7253fa68403d739da1ebcbc
jabed <jabedhasan21@gmail.com>
2015-05-14 00:19:30 +0600

    Remove Unused Class

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,9 +24,7 @@ class Kernel extends HttpKernel {
 	 * @var array
 	 */
 	protected $routeMiddleware = [
-		'auth' => 'App\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
-		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
 	];
 
 }
